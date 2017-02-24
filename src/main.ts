af40d45ea3a6da1bf175ff9f1a0abe3e2d0d7bc7
Valerio Pipolo <valep27@gmail.com>
2017-02-24 23:57:20 +0100

    Move canvas code to setup graphics function

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,15 @@
 import * as cpu from './cpu';
 
-var emulator : cpu.Chip8;
-var frameHandle : NodeJS.Timer;
+let emulator : cpu.Chip8;
+let frameHandle : NodeJS.Timer;
+let canvas : HTMLCanvasElement;
 
 export function init() {
+	setupCanvas();
+
 	emulator = new cpu.Chip8();
 	emulator.loadRomFile('roms/PONG');
 
-	let canvas = document.createElement('canvas');
-	canvas.height = cpu.SCREEN_HEIGHT * 10;
-	canvas.width = cpu.SCREEN_WIDTH * 10;
-
-	document.getElementById('container').appendChild(canvas);
-
-	let ctx = canvas.getContext('2d');
-	let imgData = ctx.createImageData(cpu.SCREEN_WIDTH, cpu.SCREEN_HEIGHT);
-	let imgSize = imgData.data.length;
-
-	let swap = false;
-	for (let i = 0; i < imgSize; i += 4) {
-		let color = swap ? 0 : 255;
-		swap = !swap;
-		imgData.data[i] = color;
-		imgData.data[i+1] = color;
-		imgData.data[i+2] = color;
-		imgData.data[i+3] = 255;
-	}
-
-	ctx.putImageData(imgData, 0, 0, 0, 0, canvas.width, canvas.height);
 	frameHandle = setInterval(frame, 33);
 
 	window.addEventListener("keydown", emulator.handleKeyDownEvent);
@@ -40,4 +22,25 @@ function frame() {
 	} catch (error) {
 		clearInterval(frameHandle);
 	}
+}
+
+function setupCanvas() {
+	canvas = document.createElement('canvas');
+	canvas.height = cpu.SCREEN_HEIGHT * 10;
+	canvas.width = cpu.SCREEN_WIDTH * 10;
+
+	document.getElementById('container').appendChild(canvas);
+
+	let ctx = canvas.getContext('2d');
+	let imgData = ctx.createImageData(cpu.SCREEN_WIDTH, cpu.SCREEN_HEIGHT);
+	let imgSize = imgData.data.length;
+
+	for (let i = 0; i < imgSize; i += 4) {
+		imgData.data[i] = 255;
+		imgData.data[i+1] = 255;
+		imgData.data[i+2] = 255;
+		imgData.data[i+3] = 255;
+	}
+
+	ctx.putImageData(imgData, 0, 0, 0, 0, canvas.width, canvas.height);
 }
